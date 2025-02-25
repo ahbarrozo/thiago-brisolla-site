5abc137cdbabb450c66c49a9f002a899e7585e8a
Alexandre Hernandes Barrozo <baal@pop-os.localdomain>
2025-02-25 09:50:21 +0100

    Include new components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
+    import { aboutSections } from "../assets/About.mockdata";
+    import { albums } from "../assets/Discography.mockdata";
     import { blogPosts } from "../assets/Blog.mockdata";
-    import Blog from "../components/Blog.svelte";
+    import About from "../components/About.svelte";
+    import Blog from "../components/Blog.svelte";  
+	import Discography from "../components/Discography.svelte";
 </script>
 
+<div class="divider mt-0"></div>
+<h2 id="diario" class="text-4xl">DIÁRIO</h2>
 <Blog blogPosts={blogPosts} />
 <div class="divider"></div>
+<h2 id="bio" class="text-4xl">QUEM SOU EU</h2>
+<About aboutSections={aboutSections} />
+<div class="divider"></div>
+<h2 id="obra" class="text-4xl">OBRA</h2>
+<Discography albums={albums} /> 
+
+<style>
+    h2 {
+        margin-bottom: 2rem; 
+    }
+</style>
