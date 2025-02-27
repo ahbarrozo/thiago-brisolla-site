a5c369f8014f2c182bdf75e8765f721e104a2cd0
Alexandre Hernandes Barrozo <baal@pop-os.localdomain>
2025-02-27 17:49:32 +0100

    Cover component

---
+++ src/components/Cover.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    interface CoverProps {
+        cover: { image: string };
+    }
+
+    const { cover }: CoverProps = $props();
+</script>
+
+<div class="h-220 overflow-hidden">
+
+<div class="relative flex justify-center bg-black opacity-20">
+    {#if cover.image}
+            <img src={cover.image} 
+                 alt="Capa do site"
+                 class="h-300 object-cover mt-[-50px]" />
+    {/if}
+</div>
+
+<div class="absolute top-[400px] 
+            left-[-200px] md:left-[-250px] 
+            lg:left-[-150px] xl:left-[-100px]
+            2xl:left-[50px] rotate-270">
+    <h2 class="text-[120px] text-nowrap">Thiago Brisolla</h2>
+    <p class="subheading">Uma frase de efeito aqui</p>
+</div>
+</div>
+
