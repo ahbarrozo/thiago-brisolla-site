023774bb65474ae50542ed793e3b52125202fe0c
Alexandre Hernandes Barrozo <baal@pop-os.localdomain>
2025-02-27 17:50:04 +0100

    Include Contact types

---
+++ src/types/Contact.types.ts
@@ -0,0 +1,8 @@
+export interface Contact {
+    id: number;
+    name: string;
+    contact: string;
+    mail: string;
+    phone: string;
+    address?: string;
+}
---
+++ src/types/SocialMedia.types.ts
@@ -0,0 +1,5 @@
+export interface SocialMedia {
+    id: number;
+    name: string;
+    link: string;
+}
