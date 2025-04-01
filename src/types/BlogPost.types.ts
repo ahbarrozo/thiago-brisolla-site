76a42bc20156a9812b44da01eb247f3d29a6183d
ahbarrozo <barrozo@tutanota.com>
2025-04-01 21:38:50 +0200

    Formatting

--- src/types/BlogPost.types.ts
+++ src/types/BlogPost.types.ts
@@ -1,11 +1,11 @@
 import type { Image } from "./Image.types";
 
 export interface BlogPostProps {
-  id?: number;
-  date: string;
-  title: string;
-  subtitle?: string;
-  text: string;
-  images: Image[];
-  isFirst?: boolean;
+    id?: number;
+    date: string;
+    title: string;
+    subtitle?: string;
+    text: string;
+    images: Image[];
+    isFirst?: boolean;
 }
